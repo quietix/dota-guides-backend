c8601cad6a8b97ba3cfd41edc1307bf4a19d1e37
quietix <i.o.zashchyk@gmail.com>
2024-09-22 16:20:15 +0300

    fix: migration fixes

---
+++ src/default_dota_app/migrations/0037_alter_attribute_unique_together_and_more.py
@@ -0,0 +1,21 @@
+import default_dota_app.models.attribute_model
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('default_dota_app', '0036_alter_attribute_unique_together'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='attribute',
+            name='display_order',
+            field=models.IntegerField(
+                default=default_dota_app.models.attribute_model.get_next_display_order,
+                validators=[django.core.validators.MinValueValidator(1)],
+            ),
+        ),
+    ]
