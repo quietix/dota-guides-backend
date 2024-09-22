850b45a46cb62b57a1d7a32ebf47bb43981e6963
quietix <i.o.zashchyk@gmail.com>
2024-09-22 14:13:34 +0300

    tmp

--- src/default_dota_app/models/attribute_model.py
+++ src/default_dota_app/models/attribute_model.py
@@ -1,28 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import Max
 
 
 class Attribute(models.Model):
-    STRENGTH = "STRENGTH"
-    AGILITY = "AGILITY"
-    INTELLIGENCE = "INTELLIGENCE"
-    UNIVERSAL = "UNIVERSAL"
-
-    ATTRIBUTE_CHOICES = {
-        STRENGTH: 'Strength',
-        AGILITY: 'Agility',
-        INTELLIGENCE: 'Intelligence',
-        UNIVERSAL: 'Universal',
-    }
-
-    attribute_name = models.CharField(
-        max_length=20,
-        choices=ATTRIBUTE_CHOICES,
-        default=UNIVERSAL)
-
+    attribute_name = models.CharField(max_length=30)
     img = models.ImageField(null=True, blank=True, upload_to='attributes')
+    display_order = models.IntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
 
     def __str__(self):
         return f"{self.attribute_name}"
 
-    def get_attribute_name(self):
-        return dict(self.ATTRIBUTE_CHOICES).get(self.attribute_name, self.attribute_name)
+    class Meta:
+        ordering = ['display_order']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # If this is a new object
+            max_order = Attribute.objects.aggregate(Max('display_order'))['display_order__max'] or 0
+            self.display_order = max_order + 1
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def reorder(cls):
+        for index, obj in enumerate(cls.objects.order_by('display_order'), start=1):
+            obj.display_order = index
+            obj.save()
