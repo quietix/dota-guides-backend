165e07cf1a5cbf1aa87a733cfde0c61b5d189952
quietix <i.o.zashchyk@gmail.com>
2024-09-28 22:41:15 +0300

    fix(db): made items unique

--- src/default_dota_app/models/item_related/item_model.py
+++ src/default_dota_app/models/item_related/item_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Item(models.Model):
-    item_name = models.CharField(max_length=200)
+    item_name = models.CharField(max_length=200, unique=True)
     item_description = models.TextField(blank=True)
     img = models.ImageField(null=True, blank=True, upload_to='items')
 
