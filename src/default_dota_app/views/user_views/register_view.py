483f5947154e979c677aa5fb96b37ea9dbb624d5
quietix <i.o.zashchyk@gmail.com>
2024-10-26 13:00:13 +0300

    fix: registration fixes

--- src/default_dota_app/views/user_views/register_view.py
+++ src/default_dota_app/views/user_views/register_view.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
 from default_dota_app.serializers import RegisterSerializer
-from rest_framework.permissions import AllowAny
+from default_dota_app.repositories import UserRepository
+from default_dota_app.auth.is_authenticated_manual_check import IsAuthenticatedManualCheck
+from rest_framework import views as drf_views
 from rest_framework import status
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
@@ -11,10 +11,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
+class RegisterView(drf_views.APIView):
+    authentication_classes = [IsAuthenticatedManualCheck]
 
     @swagger_auto_schema(
         request_body=RegisterSerializer,
@@ -25,16 +23,13 @@ class RegisterView(generics.CreateAPIView):
         operation_summary="Register a new user",
         operation_description="Create a new user account with the provided credentials."
     )
-    def create(self, request, *args, **kwargs):
-        logger.info(f"Registration attempt for data: {request.data}")
+    def post(self, request):
+        if UserRepository.is_user_authed(request):
+            return Response({"details": "Authorized user cannot register."}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = self.get_serializer(data=request.data)
+        serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-
-        logger.info(f"User {user.username} has been successfully registered.")
+        serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def perform_create(self, serializer):
-        serializer.save()
