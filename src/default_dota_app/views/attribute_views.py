f684fb5cda48a288419f97a02dbb669ead11d66b
quietix <i.o.zashchyk@gmail.com>
2024-10-26 11:20:58 +0300

    fix: fixed attributes get

--- src/default_dota_app/views/attribute_views.py
+++ src/default_dota_app/views/attribute_views.py
@@ -1,11 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
 from default_dota_app.models import *
 from default_dota_app.serializers import *
 
 
 class AttributesView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
     def get(self, request):
         attributes = Attribute.objects.all()
         serializer = AttributeSerializer(attributes, many=True)
